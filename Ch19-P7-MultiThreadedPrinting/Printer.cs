81c9db355c75c69f46cb3b8f11945494c98dc37c
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-30 18:22:07 +0500

    final code update from head office

---
+++ Ch19-P7-MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ch19_P7_MultiThreadedPrinting
+{
+    class Printer
+    {
+        private static readonly object locker = new object();
+        public void PrintNumbers()
+        {
+            lock (locker)
+            {
+                  // Display Thread info.
+                  Console.WriteLine("-> {0} is executing PrintNumbers()",
+                                              Thread.CurrentThread.Name);
+                  // Print out numbers.
+                  Console.Write("\n Your numbers:  " );
+                  for (int i = 0; i < 10; i++)
+                  {
+                      // Put thread to sleep for a random amount of time.
+                      Random r = new Random();
+                      Thread.Sleep(500 * r.Next(5));
+                      Console.Write("{0}, ", i);
+                  }
+                  Console.WriteLine();
+            }
+        }
+    }
+}
--- Ch19-P7-MultiThreadedPrinting/Program.cs
+++ Ch19-P7-MultiThreadedPrinting/Program.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ch19_P7_MultiThreadedPrinting
 {
     class Program
     {
+        private static readonly object locker = new object();
+        public static void Method1()
+        {
+            Console.WriteLine(" Long running task in progress ");
+            Thread.Sleep(3000);
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("*****Synchronizing Threads *****\n");
+            Printer p = new Printer();
+            // Make 10 threads that are all pointing to the same
+            // method on the same object.
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+                {
+                    Name = $"Worker thread #{i}"
+                };
+            }
+            // Now start each one.
+            foreach (Thread t in threads)
+                t.Start();
+
+
+            #region lock keyword in C#
+
+
+
+            #endregion
+
+            Console.ReadLine();
         }
     }
 }
