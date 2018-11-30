ad8e2e6a7bd1c5b5fb09ffa53305ccd0fcc8129f
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-30 18:50:02 +0500

    final update

--- Ch19-P7-MultiThreadedPrinting/Printer.cs
+++ Ch19-P7-MultiThreadedPrinting/Printer.cs
@@ -5,14 +5,19 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+using System.Runtime.Remoting.Contexts;
+
 namespace Ch19_P7_MultiThreadedPrinting
 {
+    [Synchronization]
     class Printer
     {
         private static readonly object locker = new object();
         public void PrintNumbers()
         {
-            lock (locker)
+            Monitor.Enter(locker);
+            //lock (locker)
+            try
             {
                   // Display Thread info.
                   Console.WriteLine("-> {0} is executing PrintNumbers()",
@@ -27,6 +32,10 @@ namespace Ch19_P7_MultiThreadedPrinting
                       Console.Write("{0}, ", i);
                   }
                   Console.WriteLine();
+            }
+            finally
+            {
+                Monitor.Exit(locker);
             }
         }
     }
